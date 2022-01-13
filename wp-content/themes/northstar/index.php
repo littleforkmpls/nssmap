ce46b3f7ae3a6d5bc5689d94b553ff8218b23a5e
Anthony Ticknor <tony.ticknor@gmail.com>
2022-01-12 23:33:24 -0600

    remove wrapper

--- wp-content/themes/northstar/index.php
+++ wp-content/themes/northstar/index.php
@@ -1,7 +1,5 @@
-<div class="wrapper">
-    <?php get_header(); ?>
-    <div>
-        Hello Minnesota!
-    </div>
-    <?php get_footer(); ?>
+<?php get_header(); ?>
+<div>
+    Hello Minnesota!
 </div>
+<?php get_footer(); ?>
