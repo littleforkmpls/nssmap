f6972c86dc7d9744f0b45a05225391afa6acf666
Anthony Ticknor <tony.ticknor@gmail.com>
2022-01-13 00:03:28 -0600

    remove comments

--- wp-content/themes/northstar/header.php
+++ wp-content/themes/northstar/header.php
@@ -1,11 +1,9 @@
 <!doctype html>
 <html lang="en-us">
     <head>
-        <!-- ZzZzZz  -->
         <meta charset="<?php bloginfo('charset'); ?>">
         <meta name="viewport" content="width=device-width,initial-scale=1" />
 
-        <!-- WP Head Injection -->
         <?php wp_head(); ?>
     </head>
     <body>
