060a077326547c7d8476163530653d94ae7109ae
Anthony Ticknor <tony.ticknor@gmail.com>
2022-01-21 13:54:47 -0600

    removing recent stories in favor of six featured stories

--- app/wp-content/themes/northstar/front-page.php
+++ app/wp-content/themes/northstar/front-page.php
@@ -45,37 +45,7 @@
             </div>
             <div class="feature__bd">
                 <ul class="blocks">
-                    <?php for ($i = 1; $i <= 3; $i++) { ?>
-                    <li>
-                        <a class="d-block">
-                            <div class="card">
-                                <div class="card__media">
-                                    <img class="d-block" src="https://via.placeholder.com/400" alt="First Avenue" />
-                                </div>
-                                <div class="card__title">
-                                    <h3 class="txt txt--hdg4 txt--upper txt--bold">First Avenue</h3>
-                                </div>
-                                <div class="card__meta">
-                                    Minneapolis, MN
-                                </div>
-                            </div>
-                        </a>
-                    </li>
-                    <?php } ?>
-                </ul>
-            </div>
-        </div>
-    </div>
-
-    <?php // recent stories ?>
-    <div class="section">
-        <div class="feature">
-            <div class="feature__hd">
-                <h2 class="txt txt--hdg3">Recent <span class="txt txt--upper txt--color-crimson">Stories</span></h2>
-            </div>
-            <div class="feature__bd">
-                <ul class="blocks">
-                    <?php for ($i = 1; $i <= 3; $i++) { ?>
+                    <?php for ($i = 1; $i <= 6; $i++) { ?>
                     <li>
                         <a class="d-block">
                             <div class="card">
