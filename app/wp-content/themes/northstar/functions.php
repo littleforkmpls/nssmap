801e2c09ab12405d4c23d6b47ff45a09fff9ccfc
Anthony Ticknor <tony.ticknor@gmail.com>
2022-01-21 11:21:07 -0600

    add support for svg files via theme functions file

--- app/wp-content/themes/northstar/functions.php
+++ app/wp-content/themes/northstar/functions.php
@@ -107,10 +107,49 @@ if( function_exists('acf_add_options_page') ) {
 }
 
 /* ====================================================================================================
- Enable Novo Map on just Posts
+ Enable Novo Map on Posts
 ==================================================================================================== */
 add_filter( 'novo_map_allowed_post_type', 'novo_map_post_types' );
 function novo_map_post_types($types) {
     $types = array('post');
     return $types;
 }
+
+/* ====================================================================================================
+ Enable Support for uploading SVGs to Media Library
+==================================================================================================== */
+add_filter('wp_check_filetype_and_ext', function($data, $file, $filename, $mimes) {
+
+    global $wp_version;
+
+    if ($wp_version !== '4.7.1') {
+        return $data;
+    }
+
+    $filetype = wp_check_filetype( $filename, $mimes);
+
+    return [
+        'ext'             => $filetype['ext'],
+        'type'            => $filetype['type'],
+        'proper_filename' => $data['proper_filename']
+    ];
+
+}, 10, 4);
+
+function cc_mime_types($mimes){
+    $mimes['svg'] = 'image/svg+xml';
+    return $mimes;
+}
+
+add_filter('upload_mimes', 'cc_mime_types');
+
+function fix_svg() {
+    echo '<style type="text/css">
+        .attachment-266x266, .thumbnail img {
+             width: 100% !important;
+             height: auto !important;
+        }
+        </style>';
+}
+
+add_action( 'admin_head', 'fix_svg' );
