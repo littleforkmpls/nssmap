8a69c0288c095970ea3b7fb4ae19e0459c64527d
Anthony Ticknor <tony.ticknor@gmail.com>
2022-02-01 11:42:22 -0600

    refactor story cards to use category instead of sticky and DRY out code by used a shared include for the template

--- app/wp-content/themes/northstar/front-page.php
+++ app/wp-content/themes/northstar/front-page.php
@@ -44,41 +44,13 @@
                 <ul class="blocks">
                     <?php
                         $args_featuredPosts = array(
-                            'post__in' => get_option('sticky_posts'),
+                            'category_name' => 'featured',
                         );
                     ?>
                     <?php $query_featuredPosts = new WP_Query($args_featuredPosts); ?>
-                    <?php while ($query_featuredPosts->have_posts()) : $query_featuredPosts->the_post(); ?>
-                    <li>
-                        <a href="<?php the_permalink(); ?>" class="d-block">
-                            <div class="card">
-                                <div class="card__media">
-                                    <?php
-                                        $img_featuredPost = get_the_post_thumbnail_url($postID, 'card-image-size');
-                                        $imgAlt_featuredPost = get_the_title();
-
-                                        if ($img_featuredPost == '') {
-                                            $img_featuredPost = get_bloginfo('template_directory') . '/assets/images/no-image-card.jpg';
-                                            $imgAlt_featuredPost = "No Image Available";
-                                        }
-
-                                    ?>
-                                    <img class="d-block" width="400" height="400" src="<?php echo $img_featuredPost; ?>?" alt="<?php echo $imgAlt_featuredPost; ?>" />
-                                </div>
-                                <div class="card__title">
-                                    <h3 class="txt txt--hdg5 txt--upper txt--bold txt--truncated"><?php the_title(); ?></h3>
-                                </div>
-                                <div class="card__meta">
-                                    <?php if( get_field('story_city') ): ?>
-                                        <?php the_field('story_city'); ?>
-                                    <?php endif; ?>
-                                    <?php if( get_field('story_state') ): ?>
-                                        <?php the_field('story_state'); ?>
-                                    <?php endif; ?>
-                                </div>
-                            </div>
-                        </a>
-                    </li>
+                    <?php while ($query_featuredPosts->have_posts()) : ?>
+                        <?php $query_featuredPosts->the_post(); ?>
+                        <?php get_template_part('includes/story-card'); ?>
                     <?php endwhile; ?>
                     <?php wp_reset_postdata(); ?>
                 </ul>
