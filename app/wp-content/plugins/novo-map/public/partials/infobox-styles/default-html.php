9477be803a7f76f4b2bcc0a629ab8e0d5df6860c
Anthony Ticknor <tony.ticknor@gmail.com>
2022-02-10 07:08:36 -0600

    change map to use post thumbnail instead of novomap assigned image

--- app/wp-content/plugins/novo-map/public/partials/infobox-styles/default-html.php
+++ app/wp-content/plugins/novo-map/public/partials/infobox-styles/default-html.php
@@ -1,8 +1,8 @@
 '<div class="infobox__content">
     <div class="infobox__content__media">
-        <?php if ($marker->infobox_image() != -1 and $marker->infobox_image() != 0) { ?>
-            <?php $wp_attachment_image_src = wp_get_attachment_image_src($marker->infobox_image(),'thumbnail'); ?>
-            <img src="<?php echo $wp_attachment_image_src[0]; ?>" alt="<?php echo esc_attr_e($marker->title()); ?>">
+        <?php $infobox_thumbnail = get_the_post_thumbnail_url($marker->post_id(),'thumbnail'); ?>
+        <?php if ($infobox_thumbnail) { ?>
+            <img src="<?php echo $infobox_thumbnail; ?>" alt="<?php echo esc_attr_e($marker->title()); ?>">
         <?php } else { ?>
             <img src="<?php echo get_bloginfo('template_directory') . '/assets/images/no-image-card-thumbnail.jpg'?>" alt="No Image Available">
         <?php } ?>
