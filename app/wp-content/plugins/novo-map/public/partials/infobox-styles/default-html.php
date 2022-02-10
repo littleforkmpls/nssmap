0f06832ad5cd31bca5b53995864b6a13cf3aa398
Anthony Ticknor <tony.ticknor@gmail.com>
2022-02-10 00:14:27 -0600

    first pass at map popups

--- app/wp-content/plugins/novo-map/public/partials/infobox-styles/default-html.php
+++ app/wp-content/plugins/novo-map/public/partials/infobox-styles/default-html.php
@@ -1,15 +1,23 @@
-'<div class="infobox-content">
-    <a href="<?php echo get_permalink($marker->post_id()) ?>">
-        <div class="title">
-            <?php esc_attr_e($marker->title()) ?>
+'<div class="infobox__content">
+    <div class="infobox__content__media">
+        <?php if ($marker->infobox_image() != -1 and $marker->infobox_image() != 0) { ?>
+            <?php $wp_attachment_image_src = wp_get_attachment_image_src($marker->infobox_image(),'thumbnail'); ?>
+            <img src="<?php echo $wp_attachment_image_src[0]; ?>" alt="<?php echo esc_attr_e($marker->title()); ?>">
+        <?php } else { ?>
+            <img src="<?php echo get_bloginfo('template_directory') . '/assets/images/no-image-card.jpg'?>" alt="No Image Available">
+        <?php } ?>
+    </div>
+    <div class="infobox__content__bd">
+        <div class="infobox__content__bd__title">
+            <a class="d-block" href="<?php echo get_permalink($marker->post_id()); ?>">
+                <div class="txt txt--family-sans txt--hdg5 txt--upper txt--bold txt--truncated">
+                    <?php echo esc_attr_e($marker->title()); ?>
+                </div>
+            </a>
         </div>
-    </a>
-    <?php if($marker->infobox_image() != -1 and $marker->infobox_image() != 0) { ?>
-        <img src="<?php
-        $wp_attachment_image_src = wp_get_attachment_image_src($marker->infobox_image());
-        echo $wp_attachment_image_src[0] ?>">
-    <?php } ?>
-    <div class="description">
-        <?php echo addslashes($marker->infobox_description()) ?>
+        <div class="infobox__content__meta">
+            <?php the_field('story_city',$marker->post_id());?>
+            <?php the_field('story_state',$marker->post_id());?>
+        </div>
     </div>
 </div>'
