eadcc2bb56c05cb3c6c8fd5339c2f7ecdac7bed1
Anthony Ticknor <tony.ticknor@gmail.com>
2022-03-17 20:40:24 -0500

    add generic page styles

--- app/wp-content/themes/northstar/page.php
+++ app/wp-content/themes/northstar/page.php
@@ -1,13 +1,34 @@
-<?php /* Template Name: General Page Template */ ?>
-
-<?php // T&Cs, About, etc ?>
+<?php // Generic Pages: T&Cs, About, etc ?>
 
 <?php get_header(); ?>
 <div class="main" role="main">
 
-    <div class="section">
-        <h1>Page Title</h1>
-    </div>
+    <h1 class="isVisuallyHidden"><?php the_title(); ?></h1>
+
+    <?php if (get_field('page_hero_image')): ?>
+        <div class="section">
+            <div class="billboard">
+                <div class="billboard__bd">
+                    <?php
+                        $img_hero = wp_get_attachment_image_url(get_field('page_hero_image'), 'large');
+                    ?>
+                    <img class="d-block" src="<?php echo $img_hero; ?>" alt=""" />
+                </div>
+            </div>
+        </div>
+    <?php endif; ?>
+
+    <?php if (get_field('page_main_content')): ?>
+        <div class="section">
+            <div class="feature feature--snowflake">
+                <div class="feature__bd">
+                    <div class="userContent">
+                        <?php the_field('page_main_content'); ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <?php endif; ?>
 
 </div>
 <?php get_footer(); ?>
