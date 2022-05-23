3487d81da1358630389ec7717f7ca7e6f9e41214
Christopher Mason <crstffr+gitlab@gmail.com>
2022-05-23 15:02:48 -0400

    Update test.sh, increase Mocha slow and timeout settings

--- aws/scripts/test.sh
+++ aws/scripts/test.sh
@@ -4,8 +4,8 @@ npx mocha \
   --bail \
   --exit \
   --exclude node_modules \
-  --slow 2000 \
-  --timeout 10000 \
+  --slow 5000 \
+  --timeout 20000 \
   "$@" \
   "./test/global.test.js" \
   "./lib/**/*.test.js"
